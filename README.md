d48270e9a6947a35fd8853e41ce6e066df43e4dc
Deyvyd Moura <53580499+deyvyd@users.noreply.github.com>
2026-07-06 23:17:30 +0000

    adiciona `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Criando repositório teste para clonar o tabnews
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Te amo, Gi! Se tu me ama, dá uma risadinha! 😄</h1>
+  return <h1>Te amo, Gi! Se tu me ama, dá uma risadinha! 😄</h1>;
 }
 
 export default Home;
